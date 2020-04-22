b0f3f0747fc631c6619f40ce997480fffef1b761
rogerscristo <rogers.cristo@gmail.com>
2020-04-22 06:09:51 -0300

    Adicionado um vetor de buffer, bem como rotinas de produção e consumo no mesmo

--- Módulo 2/prod_consumidor.c
+++ Módulo 2/prod_consumidor.c
@@ -4,32 +4,39 @@
 
 #define TRUE 1 // Define a variavel TRUE
 #define FALSE 0 // Define a variável FALSE
-#define MAX 10000000 // Tamanho do buffer.
+#define MAX 3 // Tamanho do buffer.
 
-pthread_mutex_t mutex; // Cria a variável de controle.
-pthread_cond_t full; // Cria a variável que representa o buffer cheio.
-pthread_cond_t empty; // Cria a variável que representa o buffer vazio.
-unsigned int buffer = 0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; // Cria a variável de controle.
+pthread_cond_t full = PTHREAD_COND_INITIALIZER; // Cria a variável que representa o buffer cheio.
+pthread_cond_t empty = PTHREAD_COND_INITIALIZER; // Cria a variável que representa o buffer vazio.
+unsigned int buffer[MAX];
+unsigned int insert_pos = 0;
+unsigned int remove_pos = 0;
+unsigned int num_elem = 0;
 
 /**
  * Define a função do produtor.
  */
 void *produtor(void *ponteiro) {
-    int item, i;
+    printf("Inicializando rotina de produção...\n");
 
-    for(i = 1; i <= MAX; i++) {
+    for(int i = 1; i <= MAX; i++) {
         pthread_mutex_lock(&mutex);
 
-        while(buffer != 0) {
+        while(num_elem == MAX) {
             pthread_cond_wait(&empty, &mutex);
         }
 
-        buffer = i;
-        printf("%d\n", buffer);
+        buffer[insert_pos] = i;
+        printf("Produtor: inseriu %d no buffer\n", i);
+
+        insert_pos = (insert_pos + 1) % MAX; // Reseta insert_pos para 0 quando insert_pos == MAX
+        num_elem++;
 
         pthread_cond_signal(&full);
         pthread_mutex_unlock(&mutex);
     }
+    printf("Finalizando rotina de produção...\n");
 
     pthread_exit(EXIT_SUCCESS);
 }
@@ -38,21 +45,26 @@ void *produtor(void *ponteiro) {
  * Define a função do consumidor.
  */
 void *consumidor(void *ponteiro) {
-    int item, i;
+    printf("Inicializando rotina de consumo...\n");
 
-    for(i = 1; i <= MAX; i++) {
+    for(int i = 1; i <= MAX; i++) {
         pthread_mutex_lock(&mutex);
 
-        while(buffer == 0) {
+        while(num_elem == 0) {
             pthread_cond_wait(&full, &mutex);
         }
 
-        buffer = 0;
-        printf("%d\n", buffer);
+        int payload = buffer[remove_pos];
+        printf("Consumidor: removeu %d da posição %d do buffer\n", buffer[remove_pos], remove_pos);
+
+        remove_pos = (remove_pos + 1) % MAX;
+        num_elem--;
 
         pthread_cond_signal(&empty);
         pthread_mutex_unlock(&mutex);
     }
+
+    printf("Finalizando rotina de consumo...\n");
 
     pthread_exit(EXIT_SUCCESS);
 }
