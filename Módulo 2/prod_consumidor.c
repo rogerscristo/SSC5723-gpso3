a2c7c4fbcb0b25bb2483da56c933ad05525a512c
rogerscristo <rogers.cristo@gmail.com>
2020-04-22 06:12:06 -0300

    Adicionado o cabeçalho no código do produtor_consumidor

--- Módulo 2/prod_consumidor.c
+++ Módulo 2/prod_consumidor.c
@@ -1,3 +1,13 @@
+/*******************************************************************
+*
+Disciplina: SSC5723 - Sistemas Operacionais
+Grupo: gspso3 - 2020
+Repositório Github: https://github.com/rogerscristo/SSC5723-gpso3
+Alunos: Rogers Silva de Cristo          10355930
+        Cairo Mateus Neves Ribeiro      11925833
+*
+******************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
